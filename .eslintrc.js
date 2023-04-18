806cf7105face9426f8652b08435e7e32a951e22
V Morris <digidarkdev@gmail.com>
2023-04-18 10:29:48 -0700

    chore: initialize repository with husky, eslint, commitizen, commitlint
    Added dependencies and set config files for commitlint, eslint, and husky. Deleted several default
    create-react-app files not needed for initial setup.

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'no-tabs': 0,
+  },
+};
