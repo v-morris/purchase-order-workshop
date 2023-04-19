7ed4e5cb43161234992ac47050e92ec1a9543065
V Morris <digidarkdev@gmail.com>
2023-04-19 08:55:45 -0700

    feat(createorder): add Zod package and create validation schema for Create Order form
    Added validations to make Vendor and Order Title required fields with a maximum of 50 characters
    each.

--- src/CreateOrder.jsx
+++ src/CreateOrder.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
-import { useForm } from '@mantine/form';
+import { z } from 'zod';
+import { useForm, zodResolver } from '@mantine/form';
 import {
   Select, TextInput, Stack, Header, Title, Button, Grid,
 } from '@mantine/core';
 
 export default function CreateOrder() {
-  const [searchValue, onSearchChange] = React.useState('');
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const validationSchema = z.object({
+    vendor: z.string({ invalid_type_error: 'Vendor is required' })
+      .trim().min(1, { message: 'Vendor is required' })
+      .max(50, { message: 'Vendor cannot be more than 50 characters in length' }),
+    orderTitle: z.string()
+      .trim().min(1, { message: 'Order Title is required' })
+      .max(50, { message: 'Order Title cannot be more than 50 characters in length' }),
+  });
 
   const form = useForm({
     initialValues: {
       vendor: '',
       orderTitle: '',
     },
+    validate: zodResolver(validationSchema),
   });
 
   return (
-    <form onSubmit={form.onSubmit((values) => console.log(values))}>
+    <form>
       <Stack spacing="xl">
         <Grid justify="center">
           <Grid.Col span={12}>
@@ -30,11 +41,12 @@ export default function CreateOrder() {
               withAsterisk
               label="Vendor"
               maxDropdownHeight={280}
-              onSearchChange={onSearchChange}
+              onSearchChange={setSearchValue}
               searchValue={searchValue}
               nothingFound="Vendor Not Found"
               data={['React', 'Angular', 'Svelte', 'Vue']}
               size="md"
+              clearButtonProps={{ 'aria-label': 'Clear select field' }}
               {...form.getInputProps('vendor')}
             />
           </Grid.Col>
@@ -47,7 +59,12 @@ export default function CreateOrder() {
             />
           </Grid.Col>
           <Grid.Col span={3}>
-            <Button type="submit" size="md" fullWidth>
+            <Button
+              type="button"
+              size="md"
+              fullWidth
+              onClick={() => form.validate()}
+            >
               Next
             </Button>
           </Grid.Col>
