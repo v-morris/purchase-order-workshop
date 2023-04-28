ca88ae1a16d3931ab1226da64018e41c69c3f6c9
v-morris <digidarkdev@gmail.com>
2023-04-27 17:13:36 -0700

    feat(order): add zod validations to the post request for create new order

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -1,9 +1,33 @@
+const zodExpress = require('zod-express-middleware');
+const z = require('zod');
+
 const mockDatabase = [];
 const createNewOrder = async (req, res, next) => {
   try {
     const { vendor, orderTitle } = req.body;
-    mockDatabase.push({ vendor, orderTitle });
-    res.status(200).send(mockDatabase);
+    const zodSchema = z.object({
+      vendor: z
+        .string({ invalid_type_error: 'Vendor is required' })
+        .trim()
+        .min(1, { message: 'Vendor is required' })
+        .max(50, {
+          message: 'Vendor cannot be more than 50 characters in length',
+        }),
+      orderTitle: z
+        .string()
+        .trim()
+        .min(1, { message: 'Order Title is required' })
+        .max(50, {
+          message: 'Order Title cannot be more than 50 characters in length',
+        }),
+    });
+    const result = zodSchema.safeParse(req.body);
+    if (result.success) {
+      mockDatabase.push({ vendor, orderTitle });
+      res.status(200).send(mockDatabase);
+    } else {
+      zodExpress.sendError({ type: 'Body', errors: result.error }, res);
+    }
   } catch (error) {
     next(error);
   }
